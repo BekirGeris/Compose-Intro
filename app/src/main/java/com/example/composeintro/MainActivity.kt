64ade1945f62d78d95c40a4cb6dda5aee6db065e
bekirgeris <bekirgeris@marun.edu.tr>
2021-11-17 13:31:14 +0300

    +compost use

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -1,50 +1,79 @@
 package com.example.composeintro
 
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeintro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "android")
+            MainScreen(name = "android")
         }
-
-        //testFunction(5, { testFuntionLambda() })
-
-        testFunction(5){
-            testFuntionLambda()
-            println(55)
-        }
-    }
-
-    private fun testFunction(int: Int, myFunction: () -> Unit){
-        myFunction.invoke()
-    }
-
-    private fun testFuntionLambda() {
-        println("test lambda")
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomText("android")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText("world")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText("world")
+        Spacer(modifier = Modifier.padding(5.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            CustomText("android")
+            Spacer(modifier = Modifier.padding(5.dp))
+            CustomText("world")
+        }
+    }
+}
+
+@Composable
+fun CustomText(text: String){
+    Text(
+        modifier = Modifier
+            .clickable {
+                println("hello android clicked")
+            }
+            .background(color = Color.DarkGray)
+            .padding(5.dp),
+        text = "Hello $text!",
+        color = Color.White,
+        fontSize = 25.sp,
+        fontWeight = FontWeight.Black,
+        textAlign = TextAlign.Center)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeIntroTheme {
-        Greeting("Android")
-    }
+    MainScreen(name = "android")
 }
