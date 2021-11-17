32d11c38584075846df6632dedb2f5afdfcb0928
bekirgeris <bekirgeris@marun.edu.tr>
2021-11-17 11:07:16 +0300

    lamda gösterimi ile metoda fonksiyon göndermek

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -16,16 +16,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeIntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "android")
         }
+
+        //testFunction(5, { testFuntionLambda() })
+
+        testFunction(5){
+            testFuntionLambda()
+            println(55)
+        }
+    }
+
+    private fun testFunction(int: Int, myFunction: () -> Unit){
+        myFunction.invoke()
+    }
+
+    private fun testFuntionLambda() {
+        println("test lambda")
     }
 }
 
